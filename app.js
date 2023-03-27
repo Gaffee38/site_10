ceea1096d119b0fada7254e9b0b2e3105ed913b3
Gaffee38 <83159243+Gaffee38@users.noreply.github.com>
2023-03-27 13:24:57 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ function pulseBtn(){
 };
 function addValue(){
   inputText.setAttribute('value','NamePicture');
-  const attr = inputText.getAttribute('value');
-  if (attr == 'NamePicture'){
-    setTimeout(function() {
-      inputText.removeAttribute('value','NamePicture');
-    },5000)
-  }
+  // const attr = inputText.getAttribute('value');
+  // if (attr == 'NamePicture'){
+  //   setTimeout(function() {
+  //     inputText.removeAttribute('value','NamePicture');
+  //   },5000)
+  // }
 };
+function removeAttr(){
+  inputText.removeAttribute('value','NamePicture');
+}
 function pulseSecBtn(){
   secBtn.classList.toggle("animationButtons");
 };
@@ -23,12 +26,16 @@ window.onload = function(){
 }
 function interval(){
   setTimeout(pulseBtn,0);
-  setTimeout(addValue,3000);
-  setTimeout(pulseSecBtn,6000);
-  setInterval(()=> {
-    window.location.reload();
-  },9000)
+  setTimeout(addValue,2000);
+  setTimeout(pulseSecBtn,4000);
+  setTimeout(removeAttr,6000);
+  // setInterval(()=> {
+  //   window.location.reload();
+  // },9000)
 };
+setInterval(() => {
+  interval();
+}, 6000);
 
   
 
